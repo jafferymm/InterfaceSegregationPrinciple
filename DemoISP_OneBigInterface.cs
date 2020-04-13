6b239deb1d99fae15d48514572800384c3927f4a
M Jaffery <jafferymm@yahoo.com>
2020-04-12 20:23:48 -0500

    Initial Commit

---
+++ DemoISP_OneBigInterface.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+
+    public class Document
+    {
+
+    }
+
+    //One Big Interface With Lots Functionalities Attached to It
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //Only Prints, Yet has to Implement Everything Forceably
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //Logic Implemented
+        }
+
+        public void Print(Document d)
+        {
+            //UnImplemented, and Not Needed. Needs to be documented in the API
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            //UnImplemented, and Not Needed. Needs to be documented in the API
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DemoISP_OneBigInterface
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SmallerAtomicInterfaces.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+    public class SmallerAtomicInterfaces
+    {
+    }
+
+    public class PhotoCopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //Higher Order Interface 
+    public interface IIMultiFunctionDevice : IScanner, IPrinter //...
+    { 
+
+    }
+
+    public class MultiFunctionMachine : IIMultiFunctionDevice
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public interface IFax
+    {
+        void Fax(Document d);
+    }
+
+}
